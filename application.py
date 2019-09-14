8bc593cc0bd4a26c0b4c08011266259e072e9d3a
Chris <chrishum17@gmail.com>
2019-09-14 12:30:39 -0400

    front end query properties title, category, speaker, let me knwo if you wanna add more properties

--- application.py
+++ application.py
@@ -1,14 +1,30 @@
+#
 from flask import Flask, request
 from processor import Processor
+
+from indexerclient import IndexerClient
 
 app = Flask(__name__)
 processor = Processor()
 
+#front end requests
 @app.route('/', methods=['GET'])
-def search():
-    body = request.get_json(force=True)
+#
+def parse_request():
+  body = request.get_json(force=True)
+  title = body['title']
+  category = body['category']
+  speaker = body['speaker']
+  
+  # print(body['phrase'])
     # processor.search(body['phrase'])
-    return "hello"
+    # return 'testPhrase'
+# def hello_world():
+  return title
+
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
