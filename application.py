f4bd91ee8dac378279f3c7faba37cc9ffb12b8db
Chris <chrishum17@gmail.com>
2019-09-14 20:38:42 -0400

    tryna pull

--- application.py
+++ application.py
@@ -14,16 +14,18 @@ processor = Processor()
 
 def parse_request():
   body = request.get_json(force=True)
-  title = body['title']
-  category = body['category']
-  speaker = body['speaker']
+  # title = body['title']
+  # category = body['category']
+  # speaker = body['speaker']
 
   #use this to test getting the entire json stats object thing for a video (WITH POSTMAN)
   bigJson = processor.getVideoById(TEST_VIDEO_ID)
-  return processor.organizeByKeywords(bigJson)
+  return bigJson
+  # return processor.organizeByKeywords(bigJson)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  # app.run(debug=True, host='0.0.0.0')
+  app.run(host='127.0.0.1', port=8080, debug=True)
 # end of front end requests
 
 # run this to get data and sort into json things
